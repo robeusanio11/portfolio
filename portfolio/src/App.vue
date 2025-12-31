2eef0a9ed334c08a36cf050a3aae17e69f6981ef
robeusanio11 <robeusanio11@gmail.com>
2025-12-30 17:53:20 -0800

    project setup

---
+++ portfolio/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import Navbar from './Navbar.vue'
+  import About from './About.vue'
+  import Projects from './Projects.vue'
+  import Contact from './Contact.vue'
+
+</script>
+
+<template>
+  <header>
+    <Navbar className="navbar"/>
+  </header>
+  <main>
+    <About id="about" />
+    <Projects id="projects" />
+    <Contact id="contact" />
+  </main>
+</template>
+
+<style scoped>
+div {
+  min-height: 100vh;
+  padding: 4rem;
+}
+</style>
