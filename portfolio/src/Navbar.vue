15f38f0a0869ddfcd13ac76835cde829ece680b2
robeusanio11 <robeusanio11@gmail.com>
2025-12-30 17:53:20 -0800

    project setup

---
+++ portfolio/src/Navbar.vue
@@ -0,0 +1,25 @@
+<script setup>
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id)
+    if (section) {
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  }
+</script>
+
+<template>
+    <nav class="navbar">
+      <a @click="scrollToSection('about')">About Me</a>
+      <a @click="scrollToSection('projects')">Projects</a>
+      <a @click="scrollToSection('contact')">Contact</a>
+    </nav>
+</template>
+
+<style scoped>
+a {
+    padding: 10px
+}
+</style>
---
+++ portfolio/src/Projects.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+    <div>projects</div>
+</template>
+
+<style scoped>
+</style>
