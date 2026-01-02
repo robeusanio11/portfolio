fee937b03664e4fd3f11d25d92110f1b2d0cac17
robeusanio11 <robeusanio11@gmail.com>
2026-01-02 12:26:20 -0800

    contact netlify test

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -8,9 +8,18 @@
     })
 
     // // TODO: SEND MESSAGE TO ME
-    // const submit = () => {
-    //     console.log(form.value)
-    // }
+    const handleSubmit = async (event) => {
+        const curForm = event.target;
+        const formData = new FormData(curForm);
+
+        await fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded"},
+            body: new URLSearchParams(formData).toString()
+        })
+            .then(() => console.log("Form successfully submitted"))
+            .catch(error => alert(error))
+    }
 
 </script>
 
@@ -18,7 +27,7 @@
     <div class="contact-section">
         <div class="contact-container">
             <h1 class="contact-title">Contact Me</h1>
-            <form class="contact-form" name="contact" netlify @submit.prevent="submit">
+            <form class="contact-form" name="contact" method="POST" data-netlify="true" @submit.prevent="handleSubmit">
                 <input
                     type="text"
                     id="name"
